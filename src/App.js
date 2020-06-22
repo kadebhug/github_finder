d1f632cc9c23b1d4e2b5866387d7597c92be0c7a
Kade <kadeshikaal@gmail.com>
2020-06-23 00:15:27 +0200

    testing jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  foo = () => "Shikaal";
+
+  render() {
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className="App">
+        {loading ? (
+          <h4>Loading...</h4>
+        ) : (
+          <h1>Hello {showName && this.foo()}</h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
